0187f0382bfbe30ac4057ae0d9601551a122c206
Gautam Gopal <gautamgopal9847@gmail.com>
2026-09-15 15:54:59 +0530

    Add cron job for health check every 14 minutes and update dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,9 @@ import path from "path";
 
 import { clerkMiddleware } from "@clerk/express";
 
+import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 
@@ -38,4 +40,6 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
+
+  if (process.env.NODE_ENV === "production") job.start();
 });
